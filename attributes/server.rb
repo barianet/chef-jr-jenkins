1370f16ed1e353a40d12d031b048122cf6b2da4b
Benjamin Clark <ben@benclark.com>
2013-12-06 14:13:16 -0500

    Adding some sane default attributes for the server recipe. Tuning Apache.

--- attributes/server.rb
+++ attributes/server.rb
@@ -2,3 +2,19 @@
 # Cookbook Name:: jr-jenkins
 # Attributes:: server
 #
+
+# Jenkins settings
+default['jenkins']['http_proxy']['variant'] = 'apache2'
+
+# Apache settings
+default['apache']['timeout'] = 30
+default['apache']['keepalive'] = 'Off'
+default['apache']['servertokens'] = 'Prod'
+default['apache']['serversignature'] = 'Off'
+default['apache']['traceenable'] = 'Off'
+default['apache']['prefork']['startservers'] = 4
+default['apache']['prefork']['minspareservers'] = 1
+default['apache']['prefork']['maxspareservers'] = 4
+default['apache']['prefork']['serverlimit'] = 10
+default['apache']['prefork']['maxclients'] = 10
+default['apache']['prefork']['maxrequestsperchild'] = 10_000
