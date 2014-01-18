4747d71615ee3b11967359a9f1a20ececb7ca780
Benjamin Clark <ben@benclark.com>
2014-01-18 13:59:09 -0500

    Removing Apache attribute overrides from this cookbook.

--- attributes/server.rb
+++ attributes/server.rb
@@ -13,18 +13,5 @@ default['jr-jenkins']['proxy']['ssl_certificate'] = nil
 default['jr-jenkins']['proxy']['basic_auth_user'] = nil
 default['jr-jenkins']['proxy']['basic_auth_pass'] = nil
 
-# Apache overrides
-default['apache']['timeout'] = 30
-default['apache']['keepalive'] = 'Off'
-default['apache']['servertokens'] = 'Prod'
-default['apache']['serversignature'] = 'Off'
-default['apache']['traceenable'] = 'Off'
-default['apache']['prefork']['startservers'] = 4
-default['apache']['prefork']['minspareservers'] = 1
-default['apache']['prefork']['maxspareservers'] = 4
-default['apache']['prefork']['serverlimit'] = 10
-default['apache']['prefork']['maxclients'] = 10
-default['apache']['prefork']['maxrequestsperchild'] = 10_000
-
 # Jenkins master overrides
 default['jenkins']['master']['jvm_options'] = "-Dorg.apache.commons.jelly.tags.fmt.timeZone=America/New_York"
