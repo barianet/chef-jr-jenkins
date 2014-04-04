ec78c5bef91a98cba76bfad8675d707170244633
Benjamin Clark <ben@benclark.com>
2014-04-04 16:20:08 -0400

    Use the the war installation method and pin Jenkins to 1.555 to fix authentication issue.
    * https://issues.jenkins-ci.org/browse/JENKINS-22346
    * https://github.com/opscode-cookbooks/jenkins/issues/171
    * https://github.com/opscode-cookbooks/jenkins/issues/170

--- attributes/server.rb
+++ attributes/server.rb
@@ -3,5 +3,11 @@
 # Attributes:: server
 #
 
+# Use the war installation method, pin to 1.555 to fix this issue:
+# https://github.com/opscode-cookbooks/jenkins/issues/171
+default['jenkins']['master']['install_method'] = 'war'
+default['jenkins']['master']['version'] = '1.555'
+default['jenkins']['master']['source'] = "#{node['jenkins']['master']['mirror']}/war/#{node['jenkins']['master']['version']}/jenkins.war"
+
 # Jenkins master overrides
 default['jenkins']['master']['jvm_options'] = "-Dorg.apache.commons.jelly.tags.fmt.timeZone=America/New_York"
