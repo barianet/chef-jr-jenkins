a481f505a00ee8e41a9046e6c14b42abc00db59f
Benjamin Clark <ben@benclark.com>
2014-05-02 14:41:58 -0400

    Adding job cleanup LWRP.

---
+++ providers/job_cleanup.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: jr-jenkins
+# Provider:: job_cleanup
+#
+# Author:: Ben Clark <benjamin.clark@jacksonriver.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include Jenkins::Helper
+
+action :execute do
+  Chef::Log.debug("jr_jenkins_job_cleanup: execute begin")
+
+  jobs = executor.groovy!('hudson.model.Hudson.instance.items.each() { println it.name }').split
+  Chef::Log.debug("Jenkins jobs: #{jobs}")
+  Chef::Log.debug("Jenkins jobs in whitelist: #{new_resource.whitelist}")
+
+  if !jobs.nil? && !jobs.empty?
+    # Get the diff between the real jobs and the defined jobs.
+    jobs_to_remove = jobs - new_resource.whitelist
+    Chef::Log.debug("Jenkins jobs to remove: #{jobs_to_remove}")
+  end
+
+  if !jobs_to_remove.nil? && !jobs_to_remove.empty?
+    Chef::Log.debug("jobs: #{jobs}")
+    jobs_to_remove.each do |remove_job|
+      Chef::Log.debug("Removing Jenkins job #{remove_job}")
+      executor.execute!('delete-job', remove_job)
+    end
+  end
+
+  Chef::Log.debug("jr_jenkins_job_cleanup: execute end")
+end
