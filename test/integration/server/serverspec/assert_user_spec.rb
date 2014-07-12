08dd8bcdf8abbeb14421bf0c6e2e882c002bdc17
Benjamin Clark <ben@benclark.com>
2014-07-11 22:29:40 -0400

    Initial commit of test coverage.

---
+++ test/integration/server/serverspec/assert_user_spec.rb
@@ -0,0 +1,29 @@
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+RSpec.configure do |c|
+  c.before :all do
+    c.path = '/sbin:/usr/sbin'
+  end
+end
+
+describe service('jenkins') do
+
+  it "does not allow connections from anonymous traffic" do
+    # @todo
+    skip "@todo"
+  end
+
+  it "has user 'chef'" do
+    # @todo
+    skip "@todo"
+  end
+
+  it "can authenticate as 'chef'" do
+    # @todo
+    skip "@todo"
+  end
+
+end
