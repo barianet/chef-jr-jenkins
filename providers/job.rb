1b0d09e8bbd7da0f7f5f3fd3b3f3bc1c63c32852
Dan Nunez <daniel.nunez@jacksonriver.com>
2016-02-18 14:40:57 -0500

    FC059: LWRP provider does not declare use_inline_resources

--- providers/job.rb
+++ providers/job.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+use_inline_resources
 include Jenkins::Helper
 require 'rexml/document'
 require 'shellwords'
