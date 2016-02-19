1a970bb6007b43d288af963ecf10a25e2089e08a
Dan Nunez <daniel.nunez@jacksonriver.com>
2016-02-19 15:30:58 -0500

    Point to the Jackson River Jenkins CI Repo Mirror on S3

--- attributes/server.rb
+++ attributes/server.rb
@@ -7,6 +7,8 @@
 # https://github.com/opscode-cookbooks/jenkins/issues/171
 default['jenkins']['master']['install_method'] = 'package'
 default['jenkins']['master']['version'] = '1.555-1.1'
+# Point to the Jackson River Jenkins CI Repo Mirror on S3
+default['jenkins']['master']['repository'] = 'http://jr-repo-01.s3-website-us-east-1.amazonaws.com/redhat/noarch'
 
 # Jenkins master overrides
 default['jenkins']['master']['jvm_options'] = '-Dorg.apache.commons.jelly.tags.fmt.timeZone=America/New_York'
