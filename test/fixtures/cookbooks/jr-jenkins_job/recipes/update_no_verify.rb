ab86755da1c34f0207ee2b234470439b20fdb9c9
Dan Nunez <daniel.nunez@jacksonriver.com>
2016-02-22 22:19:35 -0500

    Add test call for jr_jenkins_job_cleanup provider

--- test/fixtures/cookbooks/jr-jenkins_job/recipes/update_no_verify.rb
+++ test/fixtures/cookbooks/jr-jenkins_job/recipes/update_no_verify.rb
@@ -10,3 +10,9 @@ jr_jenkins_job 'my-job' do
   cookbook 'jr-jenkins_job'
   verify_config false
 end
+
+# Test jr_jenkins_job_cleanup
+chef_defined_jobs = ['testclient-core', 'testclient-salesforce']
+jr_jenkins_job_cleanup chef_defined_jobs do
+  only_if { node['jr-hosted-springboard']['jenkins_job_cleanup'] }
+end
