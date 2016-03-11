4ed026033b32469328654b104400623cfa3cce4b
Dan Nunez <daniel.nunez@jacksonriver.com>
2016-03-11 09:01:52 -0500

    Upgrade jr-jenkins cookbook to use jenkins 1.652-1.1 and Java 7

--- attributes/server.rb
+++ attributes/server.rb
@@ -6,9 +6,12 @@
 # Pin to 1.555-1.1 to fix this issue:
 # https://github.com/opscode-cookbooks/jenkins/issues/171
 default['jenkins']['master']['install_method'] = 'package'
-default['jenkins']['master']['version'] = '1.555-1.1'
+default['jenkins']['master']['version'] = '1.652-1.1'
 # Point to the Jackson River Jenkins CI Repo Mirror on S3
 default['jenkins']['master']['repository'] = 'http://jr-repo-01.s3-website-us-east-1.amazonaws.com/redhat/noarch'
 
 # Jenkins master overrides
 default['jenkins']['master']['jvm_options'] = '-Dorg.apache.commons.jelly.tags.fmt.timeZone=America/New_York'
+
+# We need Java 7 for new Jenkins
+default['java']['jdk_version'] = '7'
