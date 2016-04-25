871b7e7062121c15c3c5e4251467a86a22bb9876
Ben Clark <ben.clark@jacksonriver.com>
2016-04-25 15:47:09 -0400

    Restart Jenkins when plugins are installed/updated.

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -10,5 +10,6 @@ node['jr-jenkins']['plugins'].each do |plugin|
   plugin, version = plugin.split('=')
   jenkins_plugin plugin do
     version version if version
+    notifies :restart, 'service[jenkins]', :delayed
   end
 end
